d5abcbcc145d44e36527b142c686c9922abb6b9f
Thomas R. Holy <thomas.holy@eah-jena.de>
2025-06-30 15:06:27 +0200

    Removed "HistDownloader" import as not stable yet.

--- src/dwdown/download/__init__.py
+++ src/dwdown/download/__init__.py
@@ -1,9 +1,7 @@
 from .forecast_download import ForecastDownloader
 from .os_download import OSDownloader
-from .historical_download import HistDownloader
 
 __all__ = [
     "ForecastDownloader",
-    "OSDownloader",
-    "HistDownloader"
+    "OSDownloader"
 ]
