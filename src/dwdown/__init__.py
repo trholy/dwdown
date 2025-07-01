a64beefcc67c4e054e56433d133a80e2b84f1c88
Thomas R. Holy <thomas.holy@eah-jena.de>
2025-07-01 09:33:14 +0200

    Applied ruff fixes: importing.

--- src/dwdown/__init__.py
+++ src/dwdown/__init__.py
@@ -1,12 +1,7 @@
 __version__ = "0.2.0"
 __author__ = "Thomas R. Holy"
 
-from dwdown import data
-from dwdown import download
-from dwdown import notify
-from dwdown import processing
-from dwdown import upload
-from dwdown import utils
+from dwdown import data, download, notify, processing, upload, utils
 
 __all__ = [
     "data",
