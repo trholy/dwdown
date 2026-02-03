0c1fffcfc8be00ccae0de2d48ec90b11fbb90651
Thomas R. Holy <thomas.robert.holy@gmail.com>
2026-02-03 14:31:34 +0100

    Fixed imports.

--- src/dwdown/download/__init__.py
+++ src/dwdown/download/__init__.py
@@ -1,9 +1,11 @@
 from .forecast_download import ForecastDownloader
 from .os_download import OSDownloader
+from .historical_download import HistoricalDownloader
+from .mosmix_downloader import MOSMIX_Downloader
 
 __all__ = [
     "ForecastDownloader",
-    "OSDownloader"
     "OSDownloader",
     "HistoricalDownloader",
+    "MOSMIX_Downloader",
 ]
